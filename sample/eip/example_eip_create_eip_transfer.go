e452498e2d8d61beec2a6c21322cc03689ff2e03
chongkuitai <chongkuitai@baidu.com>
2026-03-02 17:04:51 +0800

    EIP资源转移返回transferId&resourceId

--- sample/eip/example_eip_create_eip_transfer.go
+++ sample/eip/example_eip_create_eip_transfer.go
@@ -1,6 +1,7 @@
 package eipsample
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/baidubce/baiducloud-go-sdk/core/util"
 	"github.com/baidubce/baiducloud-go-sdk/services/eip"
@@ -20,9 +21,17 @@ func CreateEipTransfer() {
 		TransferResourceList: []*string{},
 		ToUserId:             util.PtrString(""),
 	}
-	err = client.CreateEipTransfer(createEipTransferRequest)
+	result := &eip.CreateEipTransferResponse{}
+	result, err = client.CreateEipTransfer(createEipTransferRequest)
 	if err != nil {
 		// 此处仅做打印展示，请谨慎对待异常处理，在工程项目中切勿直接忽略异常。
 		fmt.Println("request failed:", err)
+		return
 	}
+	data, err := json.MarshalIndent(result, "", "    ")
+	if err != nil {
+		fmt.Println("json marshalIndent failed:", err)
+		return
+	}
+	fmt.Println(string(data))
 }
